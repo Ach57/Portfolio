be50c2241960ad9fc909eab70c98433e2d0bcc1d
Achraf <155260097+Ach57@users.noreply.github.com>
2025-01-18 14:28:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const path = require('path');
-const nodemailer = require('nodemailer');
+
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 3000;
 
